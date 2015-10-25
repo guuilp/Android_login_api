e3f71e57417835fed7ed3437c5deb4acbd45628f
Guilherme Lima Pereira <guuuilp@gmail.com>
2015-10-24 22:29:55 -0200

    Commit inicial

---
+++ app/src/main/java/app/AppConfig.java
@@ -0,0 +1,12 @@
+package app;
+
+/**
+ * Created by Guilherme on 24-Oct-15.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.1.15/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.1.15/android_login_api/register.php";
+}
